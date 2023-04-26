5116728185c54c53e1961a210683088139b58385
harada <hharada@vitalify.jp>
2023-04-26 12:25:15 +0900

    add feature （単語（ひらがな、漢字）によるタイトル検索機能）

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,26 @@ import { useState } from "react";
 
 export const App = () => {
   const [animeList, setAnimeList] = useState<Anime[]>([]); //アニメ情報をStateに設定
-  const [year, setYear] = useState<string | undefined>("2023"); //リリース情報をStateに設定
-  const [season, setSeason] = useState<string | undefined>("spring"); //リリース情報をStateに設定
+  const [year, setYear] = useState<string | undefined>("2023"); //リリース年をStateに設定
+  const [season, setSeason] = useState<string | undefined>("spring"); //リリースシーズンをStateに設定
+  const [title, setTitle] = useState<string | undefined>(""); //タイトルをStateに設定
 
-  const getData = async (year?: string) => {
-    const endpoint = "https://api.annict.com/v1/works";
+  const getData = async (year?: string  , season?:string) => {
+    const endpoint = "https://api.annict.com/v1/works";//APIリンク
     const access_token = "UVol8sjtyTLqvvAtJTRageHvztFssfsdPG3AYAoPXHY";
     const sort = "sort_watchers_count=desc";
-    // const year = "2023";
 
-    //fetch()で非同期通信を使ってリクエストとレスポンス取得を行う
+    //fetch()→非同期通信を使ってリクエストとレスポンス取得を行う
     //パラメータ参照（https://developers.annict.com/docs/rest-api/v1/works）
     const res = await fetch(
-      `${endpoint}/?filter_season=${year}-${season}&${sort}&access_token=${access_token}`
+      `${endpoint}/?filter_season=${year}-${season}&${sort}&filter_title=${title}&access_token=${access_token}`
     );
     const data = await res.json(); //json形式にする
     return data;
-
-    //テスト用URL// 'https://jsonplaceholder.typicode.com/posts');
   };
 
   const onSearch = async () => {
-    const data = await getData(year);
+    const data = await getData(year,season);
     setAnimeList(data.works); //'works'の中にデータがあるため指定する
     console.log(animeList);
   };
@@ -38,6 +36,7 @@ export const App = () => {
         value={year}
         onChange={(e) => setYear(e.target.value)}
       >
+        <option value="">全て</option>
         <option value="2020">2020年</option>
         <option value="2021">2021年</option>
         <option value="2022">2022年</option>
@@ -54,8 +53,8 @@ export const App = () => {
         <option value="autumn">秋</option>
         <option value="winter">冬</option>
       </select>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} autoComplete="on"/>
       <button onClick={onSearch}>検索</button>
-      {/* <input type="text" value={text} onChange={onChangeText}/> */}
 
       {/* map()=>{} 配列を順番に処理 */}
       {animeList.map((list) => (
