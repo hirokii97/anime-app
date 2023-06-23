a56fb2a102c1d48a44ab710b248224f77e90e80d
harada <hharada@vitalify.jp>
2023-06-23 18:47:47 +0900

    ♻️cookie導入

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -4,15 +4,14 @@ import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
 import { Tab } from "@/components/Tab"
 import { HeadLine } from "@/components/HeadLine"
-
 import { Result } from "@/components/Result"
 import { useCallback, useEffect, useState } from "react"
 import { Anime } from "@/types/animes"
 import React from "react"
-import { useAtom, useSetAtom } from "jotai"
-import { favoriteListAtom } from "./atoms"
-import { useCookies } from "react-cookie"
 import { Search } from "@/components/Search"
+import { useCookies } from "react-cookie"
+import { useAtom } from "jotai"
+import { favoriteListAtom } from "./atoms"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +19,25 @@ const inter = Inter({ subsets: ["latin"] })
 function Home() {
   //検索した内容を’Search’から’Result’へ受け渡す
   const [animeList, setAnimeList] = useState<Anime[]>([])
+  const [favoriteList, setFavoriteList] = useAtom(favoriteListAtom)
+  const [cookie, setCookie] = useCookies(["CookiesList"]) //react-cookieの設定
+
+  const addCookie = () => {
+    //react-cookiesに登録
+    setCookie("CookiesList", favoriteList, {
+      maxAge: 2592000,
+      path: "/",
+    })
+  }
+ 
+    const yourFavoriteList = cookie.CookiesList
+    setFavoriteList(yourFavoriteList)
+  
+
+  useEffect(() => {
+    addCookie()
+  }, [favoriteList])
+
 
   return (
     <>
